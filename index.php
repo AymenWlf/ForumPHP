89cf645df162e9b8f3fa2856f2eb29f7a14e1f78
AymenWlf <alinzgohi@gmail.com>
2022-01-24 17:23:29 +0100

    Adding logOut method

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php 
-session_start();
-    if($_SESSION['auth'] == false)
-    {
-        header('Location: signUp.php');
-        
-    };$_SESSION['auth'] = false;
+require('actions/securityAction.php');
 ?>
 
 <!DOCTYPE html>
